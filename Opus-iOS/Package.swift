eb43a87d7621004ae449e056b08d1f1b55c482f6
Greg Cooksey <greg@zello.com>
2025-03-19 12:55:10 -0500

    Creates Swift package to distribute libopus and libogg

---
+++ Opus-iOS/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Opus-iOS",
+    products: [
+      .library(name: "opus", targets: ["opus"]),
+      .library(name: "ogg", targets: ["ogg"])
+    ],
+    targets: [
+      .binaryTarget(name: "opus", path: "../opus.xcframework"),
+      .binaryTarget(name: "ogg", path: "../ogg.xcframework")
+    ]
+)
